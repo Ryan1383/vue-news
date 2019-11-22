c9d5165eb96628118b92c3ffeed503213ebf5f4e
Ryan <chaosmos1383@gmail.com>
2019-11-22 21:23:17 +0900

    fitst

---
+++ src/views/AskView.vue
@@ -0,0 +1,36 @@
+
+
+<template>
+    <div>
+        <div v-for="item in ask" :key="item.id">{{ item.title }} </div>
+    </div>
+</template>
+
+<script>
+    /* eslint-disable */
+import { fetchAskList } from '../api/index.js'
+
+export default {
+    data(){
+        return {
+            ask: [],
+        }
+    },
+    created(){
+        var vm = this;
+        
+        fetchAskList()
+            .then(function(response){
+                console.log(response);
+                vm.ask = response.data;
+            })        
+            .catch(function(error){
+                console.log(error);
+            });
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/JobsView.vue
@@ -0,0 +1,30 @@
+
+
+<template>
+    <div>
+        <div v-for="job in jobs" :key="job.id">{{ job.title }} </div>
+    </div>
+</template>
+
+<script>
+    /* eslint-disable */
+import { fetchJobsList } from '../api/index.js'
+
+
+export default {
+    data(){
+        return {
+            jobs: [],
+        }
+    },
+    created(){
+        fetchJobsList()
+            .then( response => this.jobs = response.data )        
+            .catch( error=> console.log(error));
+    }
+}
+</script>
+
+<style>
+
+</style>
